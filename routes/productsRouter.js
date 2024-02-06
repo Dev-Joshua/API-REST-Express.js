8e6a30e4d89de150f0ad4d55d5c92771017d2551
Joshua Daniel <joshua_dup@hotmail.com>
2024-02-06 16:23:19 -0500

    CRUD usando router de express

---
+++ routes/productsRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+//Los endopoint de forma especifica deben ir antes de los endpoint dinamicos
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+//Captura de parametros que vienen por url(endpoint dinamico)
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 3',
+      price: '1000',
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
+module.exports = router;
