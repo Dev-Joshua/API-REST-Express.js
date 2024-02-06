574bf8313fdadefdc649982488dc7b3389cc0704
Joshua Daniel <joshua_dup@hotmail.com>
2024-02-06 10:47:16 -0500

    parametros query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3001;
 
@@ -12,10 +14,68 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+  // res.json([
+  //   {
+  //     name: 'Product 1',
+  //     price: '120',
+  //   },
+  //   {
+  //     name: 'Product 2',
+  //     price: '350',
+  //   },
+  //   {
+  //     name: 'Product 3',
+  //     price: '1000',
+  //   },
+  // ]);
+});
+
+//Los endopoint de forma especifica deben ir antes de los endpoint dinamicos
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+//Captura de parametros que vienen por url(endpoint dinamico)
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: '120',
+    id,
+    name: 'Product 3',
+    price: '1000',
   });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+//Parametros query
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
 });
 
 //Escuchar el puerto
