2da675efb4bcbba05b87d432e2db09117b761981
Joshua Daniel <joshua_dup@hotmail.com>
2024-03-05 15:47:23 -0500

    creo esquema servicio y ruta para categorias, usuarios y ordenes

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,9 @@
 function logErrors(error, request, response, next) {
-  console.log('logErrors');
   console.error(error);
   next(error);
 }
 
 function errorHandler(error, request, response, next) {
-  console.log('errorHandler');
   response.status(500).json({
     message: error.message,
     stack: error.stack,
