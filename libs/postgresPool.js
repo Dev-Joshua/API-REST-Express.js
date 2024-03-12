7a6ba52ed4b0cf093e2c3afcbe023c76a23789b0
Joshua Daniel <joshua_dup@hotmail.com>
2024-03-12 12:27:34 -0500

    variables de entorno

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg');
 
+const { config } = require('./../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 const pool = new Pool({
-  host: 'localhost',
-  portL: 5432,
-  user: 'root',
-  password: 'admin123',
-  database: 'my_store',
+  connectionString: URI,
 });
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   portL: 5432,
+//   user: 'root',
+//   password: 'admin123',
+//   database: 'my_store',
+// });
 
 module.exports = pool;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -14,7 +14,6 @@ const service = new ProductsService();
 
 router.get('/', async (req, res) => {
   const products = await service.find();
-
   res.json(products);
 });
 
